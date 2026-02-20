e620d161427b1d294ac6b4769103e21d9c16783a
zuzzorellone <christian.esposito96@gmail.com>
2026-02-20 19:01:29 +0100

    Primo commit

---
+++ src/main/java/esposito/medicalCenter/MedicalCenterApplication.java
@@ -0,0 +1,13 @@
+package esposito.medicalCenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalCenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/esposito/medicalCenter/MedicalCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package esposito.medicalCenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
